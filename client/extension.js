0738b708e2fdd1f6790b10aa1e8cfb6520b14b33
mountainpenguin <pinguino.de.montana@googlemail.com>
2013-03-22 02:22:59 +0000

    Parse server JSON output, and display it as text on the LH side of the topbar On click, update information
    To-do:
    * [ ] Make 'AJAX' asynchronous
    * [ ] Update data automatically
    * [ ] Use icons instead of text (like loading bars, or a battery icon)
    * [ ] Show detailed information in dialog on click

--- client/extension.js
+++ client/extension.js
@@ -11,46 +11,62 @@ function _hideHello() {
     text = null;
 }
 
-function _showHello() {
-    if (!text) {
-        let message = Soup.Message.new("GET", "http://mpengu.in:12000/");
-        _httpSession.send_message(message)
-        text = new St.Label({ style_class: 'helloworld-label', text: message.response_body.data });
-        Main.uiGroup.add_actor(text);
-    }
+function _getStatus() {
+    let message = Soup.Message.new("GET", "http://mpengu.in:12000/");
+    _httpSession.send_message(message);
+    return JSON.parse(message.response_body.data);
+}
 
-    text.opacity = 255;
+function _updateText() {
+    label = button.get_children()[0];
+    label.set_text(_generateText(_getStatus()));
+}
 
-    let monitor = Main.layoutManager.primaryMonitor;
+function _generateText(data) {
+    let text = ""; 
+    text += "L: " + data.load_avg1 + ", " + data.load_avg5 + ", " + data.load_avg15;
+    text += " H: " + parseInt((data.hdd_used / data.hdd_total) * 100) + "%";
+    text += " M: " + parseInt((data.mem_used / data.mem_total) * 100) + "%";
+    text += " U: " + _parseUptime(data.uptime);
+    return text;
+}
 
-    text.set_position(Math.floor(monitor.width / 2 - text.width / 2),
-                      Math.floor(monitor.height / 2 - text.height / 2));
+function _parseUptime(seconds) {
+    var s = parseInt(seconds);
+    return _toFixed((s / (60*60*24)), 1) + " days"
+}
 
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 5,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
+function _toFixed(value, precision) {
+    var precision = precision || 0,
+    neg = value < 0,
+    power = Math.pow(10, precision),
+    value = Math.round(value * power),
+    integral = String((neg ? Math.ceil : Math.floor)(value / power)),
+    fraction = String((neg ? -value : value) % power),
+    padding = new Array(Math.max(precision - fraction.length, 0) + 1).join('0');
+    return precision ? integral + '.' +  padding + fraction : integral;
 }
 
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
-
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
+    button = new St.Bin({
+        style_class: 'panel-button',
+        reactive: true,
+        can_focus: true,
+        x_fill: true,
+        y_fill: false,
+        track_hover: true
+    });
+    let initial = _getStatus();
+    let label = new St.Label({ "text": _generateText(_getStatus()) });
+    button.set_child(label);
+    button.connect("button-press-event", _updateText);
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+    children = Main.panel._leftBox.get_children();
+    Main.panel._leftBox.insert_child_at_index(button, children.length+1);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+    Main.panel._leftBox.remove_child(button);
 }
